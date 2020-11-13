034d0011f78b3749ac71aa12913cf161ac5844bd
Daniela Blazevska <danielablazevska11@gmail.com>
2020-11-12 21:12:10 -0800

    Project2-js

---
+++ project_2_js/index.js
@@ -0,0 +1,41 @@
+
+// 1. Declare variables and use the assignment operators 
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log(b);
+console.log(b += 3); // b = b + 3
+console.log(b -= 3); // b = b - 3
+console.log(c /= 3); // c = c / 3
+console.log(b *= 3); // b = b * 3
+console.log(a %= 3); // a = ostatok od a / 3
+
+// 2. Try out the two kind of alerts that have been studied.
+
+// alert("Hello World");
+// window.alert("It's alert!");
+// window.confirm("Click me");
+
+
+//3.Try out the different types of loggings in the console
+// Bonus: Try and find out new types of loggings in the console
+
+console.table ([ "Espresso" , "Milk" , "Ice Cream"]);
+
+
+let user = {
+    user: 'Daniela Blazevska',
+    year: 2020,
+}
+console.dir(user);
+
+for ( a= 0 ; a < 5; a++)
+{
+    console.count();
+}
+
+console.warn ("This download may harm your computer");
+
+
