3a01be8335eb6fe3991f825ad1ec5190d6b420e1
Daniela Blazevska <danielablazevska11@gmail.com>
2020-11-15 23:17:34 -0800

    Homework03-js

--- project_3_js/index.js
+++ project_3_js/index.js
@@ -8,25 +8,21 @@ let z=-2;
 
 if (x>y && x>z)  
 {  
-        if (y>z)  
-        {  
-            console.log(x + ", " + y + ", " +z);  
-        }  
-        else  
-        {  
-            console.log(x + ", " + z + ", " +y);  
-        }  
+   if (y>z) {  
+        console.log(x + ", " + y + ", " +z);  
+    }  
+   else {  
+        console.log(x + ", " + z + ", " +y);  
+    }  
 }  
 else if (y>x && y >z)  
 {  
-        if (x>z)  
-        {  
-             console.log(y + ", " + x + ", " +z);  
-        }  
-        else  
-        {  
-             console.log(y + ", " + z + ", " +x);  
-        }  
+    if (x>z) {  
+         console.log(y + ", " + x + ", " +z);  
+    }  
+    else {  
+        console.log(y + ", " + z + ", " +x);  
+     }  
 }  
 else if (z>x && z>y) {  
     if (x>y) {  
